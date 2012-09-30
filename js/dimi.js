26e6bd574c5f842b4d6009cf58a47e895cc80f01
Dominik Bamberger <dmayer@suse.de>
2012-09-30 18:26:05 +0200

    fix example link

--- js/dimi.js
+++ js/dimi.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("#impress").on("click", function(event){
     $('#target').load("impress.html");
   });
-  $("#fliesen_a").on("click", function(event){
+  $("#gallery").on("click", function(event){
     $('#pic').attr("src","img/dimi_05.jpg");
   });
 });
